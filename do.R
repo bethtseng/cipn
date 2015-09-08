1531584e83179052c2dcc68fe13af79ed1e8d42e
yuting <a5520592@hotmail.com>
2015-09-08 12:21:33 +0800

    fix bugs on do.R

--- do.R
+++ do.R
@@ -9,12 +9,14 @@ do <- function(dir){
 	tmp <- arrange(tmp, CHROM)
 	#REF
 	x <- tmp[seq(4,ncol(tmp), 2)]
-	x$REF <- x[1]
+	x$m <- x[1]
 	for(i in 2:ncol(x)-1) {
-		x$REF[!is.na(x[i])] = x[i][!is.na(x[i])]
+		x$m[!is.na(x[i])] = x[i][!is.na(x[i])]
 	}
-	result <- cbind(tmp[1:3], x$REF, tmp[seq(5, ncol(tmp), 2)])
+	names(x$m) <- "REF"
+	result <- cbind(tmp[1:3], x$m, tmp[seq(5, ncol(tmp), 2)])
 	#write.table(result, file="all_merge", sep="\t", row.names=FALSE)
+	colnames(result) <- gsub(" ", "", colnames(result))
 	result
 }
 
@@ -24,21 +26,17 @@ train <- function(tbl){
 	tbl
 }
 
-#change all NA to ref
-natoref <- function(fram){
-	for(i in 5:ncol(fram)){ fram[,i][is.na(fram[,i])] = fram$REF[is.na(file[,i])] }
+#add class to data frame
+clas <- function(fram){
+	class <- c("yes", "yes", "no", "yes", "no", "yes", "no", "no","no","no","no","no","no","no","no","no","no","no","no","no")
+	fram <- cbind(fram, class)
+	#write.table(fram, file="snv_table", sep="\t", row.names=FALSE)
 	fram
 }
 
-#filtering row with same value in col
-#input data frame and col num(ex: c(5,7,8) )
-condi <- function(fram, col){
-	x <- function(fram, col1, col2){
-		con <- ifelse(as.character(fram[,col1]) == as.character(fram[,col2]), TRUE, FALSE)
-		fram <- fram[con, ][!is.na(con), ]
-	}
-	if(length(col) ==2) fram <- x(fram, col[1], col[2])
-	else fram <- x(x(fram, col[1], col[2]), col[2], col[3])
+#change all NA to ref
+natoref <- function(fram){
+	for(i in 5:ncol(fram)){ fram[,i][is.na(fram[,i])] = fram$REF[is.na(fram[,i])] }
 	fram
 }
 
@@ -47,7 +45,7 @@ ttable <- function(fram){
 	tfram <- t(fram) #read directory to data frame
 	table <- tfram[-(1:4), ]
 	colnames(table) <- paste(tfram[2,], tfram[1,], sep="_")
-	write.table(table, file="snv_table", sep="\t", row.names=FALSE)
+	#write.table(table, file="snv_table", sep="\t", row.names=FALSE)
 	table
 }
 
